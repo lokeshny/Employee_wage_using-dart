ad6a56d6de015eaae28425ea263dd55bc45de5a1
lokeshny97 <lokeshny97@gamil.com>
2022-10-20 13:44:34 +0530

    Added: parttime employee and total wage

--- employeeWage.dart
+++ employeeWage.dart
@@ -1,20 +1,39 @@
 import 'dart:math';
 
 void main(List<String> args) {
-  int IS_FULL_TIME = 1;
+  // int IS_FULL_TIME = 1;
   int EMP_RATE_PER_HOUR = 20;
+  int MAX_HRS_IN_MONTH = 100;
+  int noOfDays = 20;
   int empHrs = 0;
-  int empWage = 0;
-  Random number = Random();
-  int empCheck = number.nextInt(2);
+  int empWage = 0, totalEmpWage = 0;
+  var totalWorkingDays = 0;
+  int totalEmpHrs = 0;
+  while (totalWorkingDays < noOfDays && totalEmpHrs <= MAX_HRS_IN_MONTH) {
+    Random number = Random();
+    var empCheck = number.nextInt(3);
 
-  if (empCheck == IS_FULL_TIME) {
-    print("Employee Present..");
-    empHrs = 8;
-  } else {
-    print("Employee Absent..");
-    empHrs = 0;
+    switch (empCheck) {
+      case 0:
+        empHrs = 0;
+        print(" Absent, employee hours :${empHrs}");
+        break;
+      case 1:
+        empHrs = 8;
+        print(" Present-fulltime, employee hours :${empHrs}");
+
+        break;
+      default:
+        empHrs = 4;
+        print(" Present-parttime, employee hours :${empHrs}");
+
+        break;
+    }
+    empWage = empHrs * EMP_RATE_PER_HOUR;
+    totalEmpWage += empWage;
+    totalWorkingDays++;
+    totalEmpHrs += empHrs;
   }
-  empWage = empHrs * EMP_RATE_PER_HOUR;
-  print("Daily wage of employee = ${empWage}");
+
+  print("Total Employee Wage =  ${totalEmpWage}");
 }
