ade9148030c1c05649c5901ef258897daaa4b334
lokeshny97 <lokeshny97@gamil.com>
2022-10-20 13:35:46 +0530

    Added: Daily wage of employee

--- employeeWage.dart
+++ employeeWage.dart
@@ -1,11 +1,20 @@
 import 'dart:math';
 
-void main() {
+void main(List<String> args) {
+  int IS_FULL_TIME = 1;
+  int EMP_RATE_PER_HOUR = 20;
+  int empHrs = 0;
+  int empWage = 0;
   Random number = Random();
   int empCheck = number.nextInt(2);
-  if (empCheck == 1) {
-    print("employee present");
+
+  if (empCheck == IS_FULL_TIME) {
+    print("Employee Present..");
+    empHrs = 8;
   } else {
-    print("Employee Abseent");
+    print("Employee Absent..");
+    empHrs = 0;
   }
+  empWage = empHrs * EMP_RATE_PER_HOUR;
+  print("Daily wage of employee = ${empWage}");
 }
